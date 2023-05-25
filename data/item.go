0e8134f44f4b16d2c33ebf04ec0b38e3b206780f
agaviria <ag6402333@gmail.com>
2023-05-25 03:35:30 -0400

    Added ItemNumber field as the primary ID for each item row. Included Rate and Quantity for flexibility on calculating either an item or man hours worked.

--- data/item.go
+++ data/item.go
@@ -2,10 +2,11 @@ package data
 
 // Item is a product or a service that is billed to customer.
 type Item struct {
-	Title       string  `json:"title"`
+	ItemNumber  string  `json:"item_number"`
 	Description string  `json:"description"`
-	Price       float32 `json:"price"`
-	Quantity    float32 `json:"quantity"`
-	Tax         float32 `json:"tax"`
-	Total       float32 `json:"-"`
+	Rate        float64 `json:"rate"`
+	Quantity    float64 `json:"quantity"`
+	TaxAmount   float64 `json:"-"` // taxable amount in USD for each row item.
+	TaxRate     float64 `json:"tax_rate"`
+	SubTotal    float64 `json:"-"` // (Quantity * Rate) for each row item.
 }
